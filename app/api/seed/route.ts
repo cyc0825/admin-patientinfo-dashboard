a6d25410ab86699be20bd4f0ed321e8b8014fe08
cyc0825 <cyc_david@outlook.com>
2024-09-16 16:50:23 -0700

    Try seed DB

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -3,10 +3,6 @@ import { db, products } from 'lib/db';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  return Response.json({
-    message: 'Uncomment to seed data after DB is set up.'
-  });
-
   await db.insert(products).values([
     {
       id: 1,
